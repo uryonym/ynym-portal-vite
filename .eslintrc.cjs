d6141ad3a329b83755ed0e442f3b62b1c74dc177
yoneyama ryo <uryonym@gmail.com>
2022-12-18 22:34:14 +0900

    eslint設定を修正

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: {
+      version: "18.2.0",
+    },
+  },
+};
